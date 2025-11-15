f2b82259e821d0f527f0ddd5ca908fa47d587c63
MohamedElgemeie <mgalal2002@outlook.com>
2025-11-15 12:46:29 +0200

    update database schema

--- database/sql/create_apartment_table.sql
+++ database/sql/create_apartment_table.sql
@@ -5,11 +5,13 @@ CREATE TABLE IF NOT EXISTS apartment (
     bedrooms INT NOT NULL,
     bathrooms INT NOT NULL,
     beds INT NOT NULL,
-    description TEXT,
-    longitude DECIMAL(10, 6),
-    latitude DECIMAL(10, 6),
-    price DECIMAL(10, 2),
-    images TEXT[],
-    city city_enum,
-    country country_enum
+    description TEXT NOT NULL DEFAULT '',
+    longitude DECIMAL(10, 6) NOT NULL,
+    latitude DECIMAL(10, 6) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    city city_enum NOT NULL,
+    country country_enum NOT NULL,
+    images TEXT[] NOT NULL DEFAULT ARRAY[]::TEXT[],
+    createdAt TIMESTAMP DEFAULT NOW(),
+    updatedAt TIMESTAMP DEFAULT NOW()
 );
